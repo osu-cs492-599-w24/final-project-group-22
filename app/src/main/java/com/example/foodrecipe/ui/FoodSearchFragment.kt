d782c91fcdd75e3781ce5f1be93c4b5c34352aad
yangcan1 <yangca@oregonstate.edu>
2024-03-17 13:56:15 -0700

    button functioning

--- app/src/main/java/com/example/foodrecipe/ui/FoodSearchFragment.kt
+++ app/src/main/java/com/example/foodrecipe/ui/FoodSearchFragment.kt
@@ -2,7 +2,10 @@ package com.example.foodrecipe.ui
 
 import FoodSearchAdapter
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodrecipe.R
@@ -17,7 +20,10 @@ class FoodSearchFragment : Fragment(R.layout.fragment_recipe_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val searchBtn: Button = view.findViewById(R.id.btn_search)
+        val searchBoxET: EditText = view.findViewById(R.id.et_search_box)
         forecastListRV = view.findViewById<RecyclerView>(R.id.rv_search_results)
+
         forecastListRV.layoutManager = LinearLayoutManager(requireContext())
         forecastListRV.setHasFixedSize(true)
 
@@ -28,8 +34,15 @@ class FoodSearchFragment : Fragment(R.layout.fragment_recipe_search) {
         }
         forecastListRV.visibility = View.VISIBLE
 
-        viewModel.loadSearchResults("pasta", "7eeb9dec6924484dbd18320c7316ee6c")
+//        viewModel.loadSearchResults("pasta", "7eeb9dec6924484dbd18320c7316ee6c")
 
+        searchBtn.setOnClickListener {
+            val query = searchBoxET.text.toString()
+            if (!TextUtils.isEmpty(query)) {
+                viewModel.loadSearchResults(query, "7eeb9dec6924484dbd18320c7316ee6c")
+                forecastListRV.scrollToPosition(0)
+            }
+        }
 
 
     }
