79245bcc9aff50058f7e4e0794352868d103817f
kierrayoung <youngkie@oregonstate.edu>
2024-03-21 17:26:02 -0700

    serializable fix + calorie counter web added

--- app/src/main/java/com/example/foodrecipe/data/FoodRecipe.kt
+++ app/src/main/java/com/example/foodrecipe/data/FoodRecipe.kt
@@ -1,10 +1,10 @@
 package com.example.foodrecipe.data
 
 import com.squareup.moshi.JsonClass
-
+import java.io.Serializable
 @JsonClass(generateAdapter = true)
 data class FoodRecipe(
     val extendedIngredients: List<ExtendIngredient>,
     val description: String,
     val image: String
-)
+) : Serializable
